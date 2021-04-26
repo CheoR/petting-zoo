25e34d216d63c04dd1174c7c90a4419c36e79cd4
CheoR <cheo.roman+github@gmail.com>
2021-04-26 05:14:12 -0500

    doc: at shift to walking animals.

--- animals.py
+++ animals.py
@@ -4,15 +4,18 @@ from walks import *
 from swims import *
 from slithers import *
 
-llama = Llama("bob", "llamabama")
-cow = Cow("mary", "moocow")
-duck = Duck("qucky", "quackers")
-piggy = Piggy("dinner", "pig")
+llama = Llama("bob", "llamabama", "miday")
+cow = Cow("mary", "moocow", "miday")
+duck = Duck("qucky", "quackers", "miday")
+piggy = Piggy("dinner", "pig", "miday")
+donkey = Donkey("Danny", "donkey", "miday")
+
 fish = Fish("ickyface", "fish")
 eel = Eel("eely peely", "eel")
 croc = Croc("doc", "croc")
 aligator = Aligator("gator", "aligator")
 shark = Shark("mark", "shark")
+
 snake = Snake("jake", "snake")
 anaconda = Anaconda("ronda", "anacoda")
 worm = Worm("Orm", "worm")
--- walks/animals.py
+++ walks/animals.py
@@ -4,50 +4,56 @@ from datetime import date
 # petting are
 class Llama:
 
-    def __init(self, name, species):
+    def __init(self, name, species, shift):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
 		self.walking = True
+		self.shift = shift
 
 class Donkey:
 
-    def __init(self, name, species):
+    def __init(self, name, species, shift):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
 		self.walking = True
+		self.shift = shift
 
 class Cow:
 
-    def __init(self, name, species):
+    def __init(self, name, species, shift):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
 		self.walking = True
+		self.shift = shift
 
 class Duck:
 
-    def __init(self, name, species):
+    def __init(self, name, species, shift):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
 		self.walking = True
+		self.shift = shift
 
 class Piggy:
 
-    def __init(self, name, species):
+    def __init(self, name, species, shift):
         # Establish the properties of each animal
         # with a default value
         self.name = name
         self.species = species
         self.date_added = date.today()
 		self.walking = True
+		self.shift = shift
+
