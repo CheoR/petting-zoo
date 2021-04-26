9b8410eaa558ccb214838dc7c055b87079bb63ab
CheoR <cheo.roman+github@gmail.com>
2021-04-25 21:42:05 -0500

    petting zoo first commit

---
+++ animals.py
@@ -0,0 +1,168 @@
+from datetime import date
+
+
+# petting are
+class Llama:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Llama"
+        self.species = "Llama"
+        self.date_added = date.today()
+		self.walking = True
+
+class Donkey:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Donkey"
+        self.species = "Donkey"
+        self.date_added = date.today()
+		self.walking = True
+
+class Cow:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Cow"
+        self.species = "Cow"
+        self.date_added = date.today()
+		self.walking = True
+
+class Duck:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Duck"
+        self.species = "Duck"
+        self.date_added = date.today()
+		self.walking = True
+
+class Piggy:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Piggy"
+        self.species = "Piggy"
+        self.date_added = date.today()
+		self.walking = True
+
+class Fish:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Fish"
+        self.species = "Fish"
+        self.date_added = date.today()
+		self.swimming = True
+
+class Eel:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Eel"
+        self.species = "Eel"
+        self.date_added = date.today()
+		self.swimming = True
+
+class Croc:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Croc"
+        self.species = "Croc"
+        self.date_added = date.today()
+		self.swimming = True
+
+class Aligator:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Aligator"
+        self.species = "Aligator"
+        self.date_added = date.today()
+		self.swimming = True
+
+class Shark:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Shark"
+        self.species = "Shark"
+        self.date_added = date.today()
+		self.swimming = True
+
+class Snake:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Snake"
+        self.species = "Snake"
+        self.date_added = date.today()
+		self.slithering = True
+
+class Anaconda:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Anaconda"
+        self.species = "Anaconda"
+        self.date_added = date.today()
+		self.slithering = True
+
+class Worm:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Worm"
+        self.species = "Worm"
+        self.date_added = date.today()
+		self.slithering = True
+
+class Slime:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Slime"
+        self.species = "Slime"
+        self.date_added = date.today()
+		self.slithering = True
+
+class Tapeworm:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = "Tapeworm"
+        self.species = "Tapeworm"
+        self.date_added = date.today()
+		self.slithering = True
+
+llama = Llama()
+cow = Cow()
+duck = Duck()
+piggy = Piggy()
+fish = Fish()
+eel = Eel()
+croc = Croc()
+aligator = Aligator()
+shark = Shark()
+snak = Snake()
+anaconda = Anaconda()
+worm = Worm()
+slime = Slime()
+tapeworm = Tapeworm()
